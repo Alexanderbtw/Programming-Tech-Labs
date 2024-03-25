def894e38d25fcdf87e02f4f41000511822fd0ca
Alexander <sgoldebaev@gmail.com>
2024-03-25 22:25:03 +0300

    tests 2

---
+++ Lab2/Lab2.Tests/Task4_Tests.cs
@@ -0,0 +1,16 @@
+﻿using Lab2.Task4.Entities;
+
+namespace Lab2.Tests;
+
+public class Task4_Tests
+{
+    [Test]
+    public void TryParseReturnsTrue() 
+    {
+        // Arrange
+        string date = "1/20/2022";
+
+        // Act
+        MyDate.TryParse(date, out MyDate? result);
+    }
+}
